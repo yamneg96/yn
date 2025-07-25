6f89a55c30e27c429aa25e4e4d7e74ef6c1d5eec
Yamlak Negash <yamlaknegash96@gmail.com>
2025-07-25 09:37:52 +0300

    Fixed Deploy issues

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -27,7 +27,7 @@ export const InfiniteMovingCards = ({
 
   useEffect(() => {
     addAnimation();
-  }, []);
+  }, [addAnimation]);
   const [start, setStart] = useState(false);
   function addAnimation() {
     if (containerRef.current && scrollerRef.current) {
