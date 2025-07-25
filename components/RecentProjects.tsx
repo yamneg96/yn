9b9a46314bd137248845c7d8232a32c597832219
Yamlak Negash <yamlaknegash96@gmail.com>
2025-07-25 09:37:03 +0300

    Fixed Deploy issues

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -25,7 +25,7 @@ const RecentProjects = () => {
                     className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                     style={{ backgroundColor: "#13162D" }}
                   >
-                    <img src="/bg.png" alt="bgimg" />
+                    <img key={index} src="/bg.png" alt="bgimg" />
                   </div>
                   <img
                     src={item.img}
