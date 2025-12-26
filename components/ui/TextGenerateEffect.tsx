0ef09139b234307058582e5a70daec2227372776
Yamlak Negash <yamlaknegash96@gmail.com>
2025-12-26 13:14:42 +0300

    Updated the main color theme to blue

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -35,7 +35,7 @@ export const TextGenerateEffect = ({
               key={word + idx}
               // change here if idx is greater than 3, change the text color to #CBACF9
               className={` ${
-                idx > 6 ? "text-purple" : "dark:text-white text-black"
+                idx > 6 ? "text-blue-600" : "dark:text-white text-black"
               } opacity-0`}
             >
               {word}{" "}
