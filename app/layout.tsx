beac4e828f884ac824ce91bc3e04f83d4a648fe7
Yamlak Negash <yamlaknegash96@gmail.com>
2025-12-27 04:40:44 +0300

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+// Correct Import for Next.js 14
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -7,8 +9,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yamlak Negash",
-  description: "My Personal Portfolio",
+  title: "Yamlak Negash | Portfolio",
+  description: "MERN Stack Developer & Agentic AI Specialist Portfolio",
 };
 
 export default function RootLayout({
@@ -19,12 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link
-          rel="icon"
-          href="/yn.png"
-          sizes="any"
-          className="w-10 h-10 rounded"
-        />
+        <link rel="icon" href="/yn.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
@@ -34,6 +31,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          {/* Vercel Analytics Component */}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
